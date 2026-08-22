8884e184b1b1a87a5290767e6d861a1c0702a9b2
Carrick De Min <opindustriesc@gmail.com>
2026-08-22 10:46:09 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    double var = 10;
+    std::cout << var << std::endl;
+    std::cin >> var;
+    std::cout << var << std::endl;
+
+    const std::string str1 = "Hello, World!";
+    int num1;
+    int num2;
+
+    std::cout << "Enter two numbers to add." << std::endl;
+    std::cin >> num1;
+    std::cin >> num2;
+
+    std::cout << num1 + num2 << std::endl;
+    std::cout << sqrt(num1 + num2) << std::endl;
+    std::cout << str1 << std::endl;
+}
