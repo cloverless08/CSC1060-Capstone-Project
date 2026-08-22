0a58b4c478358e7a582a30f5d0a6d3be0a5de14a
Carrick De Min <opindustriesc@gmail.com>
2026-08-22 12:44:20 -0600

    update

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 
 using namespace std;
 
+int testFunction(int num) {
+    std::cout << "Your number is: " << num << std::endl;
+    return 0;
+}
+
 int main() {
-    /*
      double var = 10;
     std::cout << var << std::endl;
     std::cin >> var;
     std::cout << var << std::endl;
-    */
+    testFunction(var);
 
     const std::string str1 = "Hello, World!";
     int num1;
@@ -24,4 +28,6 @@ int main() {
     std::cout << num1 + num2 << std::endl;
     std::cout << sqrt(num1 + num2) << std::endl;
     std::cout << str1 << std::endl;
+
+    return 0;
 }
