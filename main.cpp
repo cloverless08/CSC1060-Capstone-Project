a93cec0deaf2ed575dc1e5e1b26b1c42d7d3651f
Carrick De Min <opindustriesc@gmail.com>
2026-08-24 12:20:08 -0600

    .

--- main.cpp
+++ main.cpp
@@ -20,9 +20,11 @@ int main()
         return -1;
     }
 
+    // establish glfw context
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window = glfwCreateWindow(
         width, height,
         WINDOW_TITLE,
