67b6e11eb82c290cf789a89917a70365a868163a
Carrick De Min <opindustriesc@gmail.com>
2026-08-25 00:03:37 -0600

    .

---
+++ build/CMakeFiles/CSC1060_Capstone_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSC1060_Capstone_Project.dir/link.d"
+  "CMakeFiles/CSC1060_Capstone_Project.dir/main.cpp.o"
+  "CMakeFiles/CSC1060_Capstone_Project.dir/main.cpp.o.d"
+  "CSC1060_Capstone_Project"
+  "CSC1060_Capstone_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSC1060_Capstone_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
