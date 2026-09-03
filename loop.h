1bf5192474db9a3c9819f2d6492f3eeeb1e59de5
Carrick De Min <opindustriesc@gmail.com>
2026-09-03 15:56:59 -0600

    populated new files, FIX ERROR IN LOOP.CPP

--- loop.h
+++ loop.h
@@ -2,7 +2,21 @@
 // Created by cdemin on 9/3/26.
 //
 
+#pragma once
+
 #ifndef CSC1060_CAPSTONE_PROJECT_LOOP_H
 #define CSC1060_CAPSTONE_PROJECT_LOOP_H
+#include <cstdint>
 
 #endif //CSC1060_CAPSTONE_PROJECT_LOOP_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bits/valarray_after.h>
+
+struct Vec2 {
+    double x, y;
+};
+
+void loop(std::vector<uint32_t> pixelBuffer, int width, int height, Vec2& rayDir, unsigned long long i);
