6a60d3227b0574d36a2c164c7064cda1ca172517
Carrick De Min <opindustriesc@gmail.com>
2026-09-10 18:25:31 -0600

    moved deprecated window loop

--- loop.cpp
+++ loop.cpp
@@ -26,3 +26,27 @@
 int loop() {
     return 0;
 }
+
+int test_loop(std::vector<uint32_t> buffer, int pixelX, int pixelY, int height, int width) { // old loop that draws a gradient as i was learning
+    // normalize pixels into UV coordinates
+    double u = (double)pixelX / height;
+    double v = (double)pixelY / width;
+
+    Vec2 rayDir = {u,v};
+
+    double length = std::sqrt(rayDir.x * rayDir.x + rayDir.y * rayDir.y); // simple pythagorean theorem yo
+    if (length > 0.0001) { // avoids NaN or division by zero at the centerpoint
+        rayDir.x /= length;
+        rayDir.y /= length;
+    }
+
+    // de-normalize, basically converts back into rgba
+    int pixelR = (int)((rayDir.x * 0.5 + 0.5) * 255);
+    int pixelG = (int)((rayDir.y * 0.5 + 0.5) * 255);
+    //int pixelB = i;
+    int pixelB = 255;
+
+    SetPixel(buffer.data(), width, height,
+        pixelX, pixelY, pixelR, pixelG, pixelB,
+        255);
+}
