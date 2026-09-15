ba639753b06a4a61f197cbdd2cfd56322acd3bb7
Carrick De Min <carrickdemin@icloud.com>
2026-09-15 13:43:04 -0600

    work on loop() bug: added diagnostic lines

--- loop.cpp
+++ loop.cpp
@@ -25,6 +25,11 @@
 #define newline "\n"
 
 int loop(std::vector<uint32_t>& buffer, BlackHole hole, int pixelX, int pixelY, const int height, const int width) { // loop for pixel color assignments
+
+    std::cout << buffer.data() << std::endl;
+    std::cout << hole.radius << std::endl;
+    std::cout << hole.mass << std::endl;
+
     // normalize pixels into UV coordinates 0 through 1
     const double xCentered = static_cast<double>((2 * pixelX - width) / height);
     const double yCentered = static_cast<double>((2 * pixelY - height) / height);
