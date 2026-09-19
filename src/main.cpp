d576e342b2ee42091bda6150fb3b42e04c0b4d61
Carrick De Min <opindustriesc@gmail.com>
2026-09-19 00:25:41 -0600

    .

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ constexpr int STANDARD_RESOLUTION_WIDTH = 640;
 constexpr int STANDARD_RESOLUTION_HEIGHT = 480;
 constexpr int MOVING_RESOLUTION_HEIGHT = 320;
 constexpr int MOVING_RESOLUTION_WIDTH = 240;
+
 const std::array<std::string, 4> msgType = {"SYSTEM", "ERROR", "SETUP", "WARN"}; // strOut() helpful labels
 const std::array<int, 4> BACKGROUND_COLOR = {255, 255, 255, 255};
 
