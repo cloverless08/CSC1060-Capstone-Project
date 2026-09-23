9f720416e2095bd6efa168376b7ea8ef8f7501c3
Carrick De Min <opindustriesc@gmail.com>
2026-09-23 00:37:05 -0600

    working on operator functions in Vec3 class

--- include/structs.h
+++ include/structs.h
@@ -28,9 +28,9 @@ public:
 
 
     // operator functions
-    Vec3 operator-() const {
-        return Vec3{-position[0], -position[1], -position[2]};
-    }
+    Vec3 operator-() const { return Vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
 
 
 };
