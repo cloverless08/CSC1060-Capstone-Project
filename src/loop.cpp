06e02074cdef4852f202442db35453c1dcb2a64c
Carrick De Min <opindustriesc@gmail.com>
2026-09-23 01:22:36 -0600

    fixed known bug by writing to e[0] and e[1] instead of x and y

--- src/loop.cpp
+++ src/loop.cpp
@@ -69,8 +69,8 @@ void loop3D(std::vector<uint32_t>& buffer,
     const int pixelA = backgroundColor[3];
 
     Vec3 ray;
-    ray.x = xPixelCentered;
-    ray.y = yPixelCentered;
+    ray.e[0] = xPixelCentered;
+    ray.e[1] = yPixelCentered;
 
     SetPixel(buffer.data(), width, height,
     xPixel, yPixel, pixelR, pixelG, pixelB,
--- src/main.cpp
+++ src/main.cpp
@@ -127,8 +127,8 @@ int main() {
         // update pixel buffer
          for (int pixelY = 0; pixelY < STANDARD_RESOLUTION_HEIGHT; pixelY++) {
              for (int pixelX = 0; pixelX< STANDARD_RESOLUTION_WIDTH; pixelX++) {
-                //loop3D(pixelBuffer, hole, pixelX, pixelY, STANDARD_RESOLUTION_HEIGHT, STANDARD_RESOLUTION_WIDTH, BACKGROUND_COLOR);
-                 test_loop(pixelBuffer, pixelX, pixelY, STANDARD_RESOLUTION_HEIGHT, STANDARD_RESOLUTION_WIDTH);
+                loop3D(pixelBuffer, hole, pixelX, pixelY, STANDARD_RESOLUTION_HEIGHT, STANDARD_RESOLUTION_WIDTH, BACKGROUND_COLOR);
+                 //test_loop(pixelBuffer, pixelX, pixelY, STANDARD_RESOLUTION_HEIGHT, STANDARD_RESOLUTION_WIDTH);
              }
          }
 
